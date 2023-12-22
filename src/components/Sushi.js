4a647bccdd89fb251f5fa1e3452359ca6e34a991
Tony Eder <tonyeder11@gmail.com>
2023-12-21 19:28:45 -0800

    first four sushi showing on front end

--- src/components/Sushi.js
+++ src/components/Sushi.js
@@ -1,20 +1,26 @@
 import React from "react";
 
-function Sushi(props) {
+function Sushi({ sushi }) {
+  const { id, name, img_url, price, created_at } = sushi
+
+  function handleClick() {
+    console.log('clicked')
+  }
+
   return (
-    <div className="sushi">
-      <div className="plate" onClick={/* Give me a callback! */ null}>
+    <div key={id} className="sushi">
+      <div className="plate" onClick={handleClick}>
         {/* Tell me if this sushi has been eaten! */}
         {false ? null : (
           <img
-            src={/* Give me an image source! */ null}
-            alt={/* Give me a name! */ "Sushi"}
+            src={img_url}
+            alt={{name} + "Sushi"}
             width="100%"
           />
         )}
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {name} - ${price}
       </h4>
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from './Sushi'
 
 function SushiContainer(props) {
+  const [ sushis, setAsSushis ] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/sushis')
+    .then((res) => res.json())
+    .then((sushis) => setAsSushis(sushis))
+  }, [] )
+  
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
+      {sushis.slice(0, 4).map((sushi) => (
+        <Sushi sushi={sushi} key={sushi.id} />
+      ))}
       <MoreButton />
     </div>
   );
