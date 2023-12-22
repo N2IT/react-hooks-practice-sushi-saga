ccf23bcecf6d0d509746a013ac8060ff8323bfbf
Tony Eder <tonyeder11@gmail.com>
2023-12-22 00:22:33 -0800

    more sushi click button configured

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -3,21 +3,34 @@ import MoreButton from "./MoreButton";
 import Sushi from './Sushi'
 
 function SushiContainer(props) {
-  const [ sushis, setAsSushis ] = useState([])
+  const [sushis, setSushis] = useState([]);
+  const [displayedSushis, setDisplayedSushis] = useState([]);
+  const [startIndex, setStartIndex] = useState(0);
 
   useEffect(() => {
     fetch('http://localhost:3001/sushis')
-    .then((res) => res.json())
-    .then((sushis) => setAsSushis(sushis))
-  }, [] )
-  
+      .then((res) => res.json())
+      .then((sushis) => {
+        setSushis(sushis);
+        setDisplayedSushis(sushis.slice(0, 4));
+      });
+  }, []);
+
+  function moreSushiClick() {
+    const endIndex = startIndex + 4;
+    const nextSushi = sushis.slice(startIndex, endIndex);
+
+    setStartIndex(endIndex);
+
+    setDisplayedSushis(nextSushi);
+  }
 
   return (
     <div className="belt">
-      {sushis.slice(0, 4).map((sushi) => (
+      {displayedSushis.map((sushi) => (
         <Sushi sushi={sushi} key={sushi.id} />
       ))}
-      <MoreButton />
+      <MoreButton moreSushiClick={moreSushiClick} />
     </div>
   );
 }
